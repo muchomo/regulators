802cf01bf106156efc86ab76492dcbae57439110
muchomo <111954358+muchomo@users.noreply.github.com>
2023-09-12 18:49:44 -0400

    updated p2e

---
+++ constants/p2eabi.js
@@ -0,0 +1,763 @@
+
+module.exports = {p2eabi: [
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_rewardsToken",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "AddedToCoachlist",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "AddedToWhitelist",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "newRate",
+				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "ChangedRewardRate",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "RemovedFromCoachlist",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "RemovedFromWhitelist",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "player",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "coach",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "bool",
+				"name": "newstatus",
+				"type": "bool"
+			}
+		],
+		"name": "changedPlayerStatus",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "newInterval",
+				"type": "uint256"
+			}
+		],
+		"name": "changedRewardTimeInterval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "depositMade",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "gameEnd",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timeEnterred",
+				"type": "uint256"
+			}
+		],
+		"name": "newActivityRecorded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "Withdrawer",
+				"type": "address"
+			}
+		],
+		"name": "tokensWithdrawn",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "newRewardBalUpdate",
+				"type": "uint256"
+			}
+		],
+		"name": "updateRewards",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_coachWallet",
+				"type": "address"
+			}
+		],
+		"name": "addCoach",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerWallet",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "playerCoach",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "jerseyNumber",
+				"type": "uint256"
+			}
+		],
+		"name": "addPlayer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "newstatus",
+				"type": "bool"
+			}
+		],
+		"name": "changePlayerStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "changeRewardRate",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newrate",
+				"type": "uint256"
+			}
+		],
+		"name": "changeRewardTimeInvertal",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "checkPendingRewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "rewards",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "coachAddresses",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "coachCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "coachProfile",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "isActive",
+				"type": "bool"
+			},
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "createdTime",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numOfPlayers",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "endGame",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gameEndTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gameStartTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getCreationTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "createdTime",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getPlayerCoach",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "playerCoach",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getPlayerJersey",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "jerseyNumber",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getStatus",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "isActive",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "isCoach",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "isWhitelisted",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "playerCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "playerProfile",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "isActive",
+				"type": "bool"
+			},
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "playerCoach",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "jerseyNumber",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "createdTime",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "pendingRewards",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "lastActivityTimestamp",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "redeem",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "removeCoach",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardInterval",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardRate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardsToken",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalLocked",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "user",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "whitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	}
+]
+}
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,20 @@
 import Head from "next/head";
-import Image from "next/image";
-import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { abi } from "../constants/abi";
-import { sabi } from "../constants/stakingabi";
-import { kabi } from "../constants/kabi";
+import {p2eabi} from "../constants/p2eabi";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { Web3Provider } from '@ethersproject/providers';
-import dynamic from "next/dynamic";
-import Link from "next/link";
 import { useToasts } from 'react-toast-notifications'
-import { ToastContainer, toast } from 'react-toastify';
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Circles from "../components/Circles";
 import Infopanel from "../components/Infopanel";
-import p2eInterface from "../components/P2eInterface";
-import regulators from '../public/regulators.png';
+
 
 
 
 // const {} = dynamic(import("tw-elements"), { ssr: false });
 
-export const injected = new InjectedConnector({ supportedChainIds: [25]});
+export const injected = new InjectedConnector({ supportedChainIds: [25,338]});
 
 export default function Home() {
   const myPromise = new Promise((resolve, reject) => {
@@ -49,12 +38,26 @@ export default function Home() {
   const [demoFantasy, setDemoFantasy] = useState(false);
   const [appFantasy, setAppDemo] = useState(false);
   const [signup, setSignup] = useState(false);
+  const [playerActive, setPlayerActive] = useState(false);
+  const [coachActive, setCoachActive] = useState(false);
+  const [managerActive, setManagerActive] = useState(false);
+  const [tokenBalance, setTokenBalance] = useState(0);
+  const [contractOwner, setContractOwner] = useState(null);
+  const [isNewUser, setIsNewUser] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
 
   const Web3 = require('web3');
-  const web3 = new Web3('https://evm.cronos.org');
+  const web3 = new Web3('https://evm-t3.cronos.org');
 
   const { addToast } = useToasts()
+//cronos testnet p2e contract address
+const p2e = "0x9d310c0dfa531efe7c357353b88ad9bc7355a6cc";
+const R1 = new web3.eth.Contract(p2eabi, p2e);
 
+  //cronos testnetwork cbz contract address
+  const tokencontractAddress='0xe871e7874d5d50db39955b728c6a74be3701163a';
+ //initialize new contract for test cbz
+ const CBZTOKENContract = new web3.eth.Contract(abi, tokencontractAddress);
 
 
 
@@ -64,14 +67,15 @@ export default function Home() {
   var Eth = require('web3-eth');
 
 // "Eth.providers.givenProvider" will be set if in an Ethereum supported browser. set to cronos testnet
-var eth = new Eth(Eth.givenProvider || 'https://evm.cronos.org');
+var eth = new Eth(Eth.givenProvider || 'https://evm-t3.cronos.org');
 
 const gasPrice = eth.getGasPrice()
 
 var o = 0;
     var z  = 0;
     var p = 0;
-    var q = 0;
+    var q = '';
+    var i = false;
     var r = 0;
     var s = 0;
     var profile = '';
@@ -91,13 +95,34 @@ var o = 0;
       }
     }
     connectWalletOnPageLoad();
+
+    if (playerActive && !coachActive && !managerActive){
+      handlePlayerActive();
+    }
+    if (!playerActive && coachActive && !managerActive){
+      handleCoachActive();
+    }
+   
+    if(!playerActive && !coachActive && managerActive){
+      handleManagerActive();
+    }
+    if(!playerActive && !coachActive && !managerActive && isNewUser){
+      handleIsNewUser();
+    }
     const addListeners = async () => {
       if(localStorage?.getItem('isWalletConnected') === 'true'){}
     }
     if (typeof window.ethereum !== "undefined") {
       setHasMetamask(true);
     }
-  }, []);
+    if(isConnected){
+      handleGetRole();
+    }
+    if(contractOwner==userAccounts[0]){
+      setManagerActive(true);
+      console.log(managerActive,contractOwner,userAccounts[0]);
+    }
+  }, [playerActive,coachActive,managerActive,contractOwner,isNewUser,isConnected]);
 
   const {
     active,
@@ -111,32 +136,66 @@ var o = 0;
  
 
   async function connect() {
- 
-    
     if (typeof window.ethereum !== "undefined") {
       try {
         setUserValuesLoading(true);
-       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-       await activate(injected);
-       setHasMetamask(true);
-       setUserAccounts(accounts);
-     
-       myPromise
-       .then(setUserValuesLoading(false))
-       .then(document.getElementById("playerAddress").innerHTML = (accounts[0]))
-       
-        
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        await activate(injected);
+        setHasMetamask(true);
+        setUserAccounts(accounts);
+  
+        await R1.methods.checkPendingRewards.call((err, result) => {
+          z = result;
+        });
+        await R1.methods.isCoach(accounts[0]).call((err, result) => {
+          i = result;
+        });
+        setCoachActive(i);
+        await R1.methods.playerProfile(accounts[0]).call((err, result) => {
+          p = result[0];
+        });
+        setPlayerActive(p);
+  
+        // Update the contractOwner with the result
+        await R1.methods.owner.call((err, result) => {
+          q = result;
+          setContractOwner(result); // Update contractOwner here
+          console.log(result);
+          if (q === accounts[0]) {
+            setManagerActive(true);
+            console.log(result);
+
+          }
+        });
+  
+        if (!i && !p && !managerActive) {
+          setIsNewUser(true);
+        }
+  
+        await CBZTOKENContract.methods.balanceOf(accounts[0]).call((err, result) => {
+          o = result / 1000000000000000000;
+        });
+        setTokenBalance(o);
+  
+        myPromise
+          .then(setUserValuesLoading(false))
+          .then(document.getElementById("playerRewardsDue").innerHTML = z)
+          .then(document.getElementById("playerTokenBalance").innerHTML = o)
+          .then(console.log(i, p, q, isNewUser, managerActive,accounts[0],contractOwner))
+          .then(document.getElementById("playerAddress").innerHTML = accounts[0]);
       } catch (e) {
         console.log(e);
-        if(e){
-          try{initValues()}
-          catch (e){
+        if (e) {
+          try {
+            initValues();
+          } catch (e) {
             console.log(e);
           }
         }
       }
     }
   }
+  
 
   async function loadValues() {
  
@@ -160,7 +219,58 @@ var o = 0;
     }
   }
 
-
+async function handleGetRole(){
+  if (active){
+    try{
+      await R1.methods.checkPendingRewards.call((err, result)=>{z = result });
+      await R1.methods.isCoach(userAccounts[0]).call((err, result)=>{i = result });
+      setCoachActive(i);
+      await R1.methods.playerProfile(userAccounts[0]).call((err, result)=>{p = result[0] });
+      setPlayerActive(p);
+      await R1.methods.owner.call((err, result)=>{q = result;if(q = userAccounts[0]){
+        setManagerActive(true);
+      } });
+      setContractOwner(q);
+      if(!i&&!p&&!managerActive){
+        setIsNewUser(true);
+      }
+      await CBZTOKENContract.methods.balanceOf(userAccounts[0]).call((err, result)=>{o = result / 1000000000000000000});
+      setTokenBalance(o);
+      myPromise
+      .then(document.getElementById("playerRewardsDue").innerHTML = z)
+      .then(document.getElementById("playerTokenBalance").innerHTML = o)
+      .then(console.log(i, p, managerActive, isNewUser))
+    }catch(e){
+      console.log(e);
+    }
+  }
+}
+async function handleIsNewUser(){
+  if(active && isNewUser){
+    setCoachActive(false);
+    setManagerActive(false);
+    setPlayerActive(false);
+  }
+}
+async function handlePlayerActive(){
+  if(playerActive){
+    setCoachActive(false);
+    setManagerActive(false);
+  }
+}
+async function handleCoachActive(){
+  if(coachActive){
+    setPlayerActive(false);
+    setManagerActive(false);
+  }
+}
+async function handleManagerActive(){
+  if(!managerActive){
+    setCoachActive(false);
+    setPlayerActive(false);
+    setManagerActive(true);
+  }
+}
   async function openToolTip(){
     if(!toolTip)
     {
@@ -192,6 +302,30 @@ async function viewAbout(){
     setAbout(true);
   }
 }
+
+   //claim rewards on 30 day contract
+   async function claimRewards() {
+    if (active) {
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(p2e, p2eabi, signer);
+      setUserValuesLoading(true);
+      try {
+       const claimHash = await contract.redeem();
+       await claimHash.wait();
+       myPromise
+       .then(addToast('TX: '+ claimHash.hash, {
+        appearance: 'success'
+       }))
+       
+        setUserValuesLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Rewards claim no works.");
+      alert(error.message);
+    }
+  }
 async function viewDemo(){
   if(!demoFantasy){
     setWelcome(false);
@@ -210,8 +344,8 @@ async function viewWelcome(){
     <div>
      
       <Head>
-        <title>E Warrior</title>
-        <meta name="description" content="E Warrior" />
+        <title>Practice to Earn</title>
+        <meta name="description" content="Cronoballz Regulators Practice to Earn" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -219,8 +353,8 @@ async function viewWelcome(){
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
           rel="stylesheet"
         />
-        <meta property="og:title" content="E Warrior" />
-		<meta property="og:description" content="E Warrior" />
+        <meta property="og:title" content="Regulators Practice to Earn" />
+		<meta property="og:description" content="Regulators Practice to Earn Claim Dapp" />
 		<meta property="og:image" content="https://cronoballz.com/wp-content/uploads/2022/12/cbzd.png" />
       </Head>
       <nav className={styles.nav_grid}>
@@ -251,32 +385,37 @@ async function viewWelcome(){
               <div className={styles.containerPadding}></div>
               <div id="infoContainer">
                 <div id="infoLeftContainer">
-                  <p className={styles.playerBalInfo}>NEXT CLAIM :<a> DATE</a></p>
+                  <p className={styles.playerBalInfo}>REWARDS DUE :</p>
+                  <p className={styles.playerBalInfo} id="playerRewardsDue">NA</p>
                 </div>
                 <div id="infoRightContainer">
-                  <p className={styles.playerBalInfo}>CBZ BALANCE : <a>0</a></p>
+                  <p className={styles.playerBalInfo}>CBZ BALANCE :</p>
+                  <p className={styles.playerBalInfo} id="playerTokenBalance">0</p>
                 </div>
               </div>
               <div className={styles.containerPadding}></div>
               </div> 
-             <div className={styles.p2esection}>
+             <div className={styles.p2esection} style={{height:'10%'}}>
               <div id="newsInfo">
                {active? <p id="playerAddress">address</p>:<p>PLEASE LOG IN</p>}
               </div>
               </div> 
-             <div className={styles.p2esection}><button id="rewardsbutton" style={{backgroundImage:'url(https://nflregulators.com/wp-content/uploads/2023/06/base.png)'}}>REWARDS</button></div> 
-             <div className={styles.p2esection}>
-              <div id="messageOfDayContainer">
-                <p id="messageOfDay">THIS IS THE MESSAGE OF THE DAY</p>
-              </div>
+             <div className={styles.p2esection}style={{height:'40%'}}>
+              <img src="regulators.png" id="playerHolder" />
               </div> 
+           
              <div className={styles.p2esection}>
               <div id="redeemContainer">
-              <button id="redeembutton">REDEEM</button></div>
+              <button id="redeembutton" onClick={claimRewards}> REDEEM</button></div>
               <div id="contactContainer">
               <p id="emailAddress">regulators@cronoballz.com
                 <a id="phoneNumber">(904) 894 - 7276</a>
               </p>
+              </div>
+              </div> 
+              <div className={styles.p2esection} style={{height:'10%'}}>
+              <div id="messageOfDayContainer">
+                <p id="messageOfDay">THIS IS THE MESSAGE OF THE DAY</p>
               </div>
               </div> 
             </>:<><p id="welcomeTitle" >PRACTICE TO EARN</p>
